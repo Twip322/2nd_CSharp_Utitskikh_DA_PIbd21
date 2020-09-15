9f4667aae8b3fe3b7756f8022bcbb67788daf4f8
Утицких Дмитрий <samprp458804@gmail.com>
2020-09-15 15:32:37 +0400

    lab 5 main

---
+++ DeliveryShop/DeliveryShopRestApi/Models/MealModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeliveryShopRestApi.Models
+{
+    public class MealModel
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
